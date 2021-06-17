6eec9be37fdfc496788332d72fa61d346dc74626
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2021-06-17 14:13:48 +0100

    Made search function work

--- src/App.js
+++ src/App.js
@@ -1,31 +1,61 @@
 import React, { useEffect, useState } from "react";
+import Recipe from "./Recipe";
 import "./App.css";
 
 const App = () => {
   const appId = "fff36917";
   const appKey = "35acc3180a5f19c4cf4a1312ccf60b9e";
 
-  useEffect(  () => {
-getRecipes();
-  }, []);
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
-const getRecipes = async () => {
-const response = await fetch(
-  `https://api.edamam.com/search?q=chicken&app_id=${appId}&app_key=${appKey}`
-);
-const data = await response.json();
-console.log(data.hits);
-}
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits)
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  }
 
   return (
     <div className="App">
       <h1>Recipe Search</h1>
-      <form className="search-form">
-        <input type="text" className="search-bar" />
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          type="text"
+          className="search-bar"
+          value={search}
+          onChange={updateSearch}
+        />
         <button type="submit" className="search-button">
           Search
         </button>
       </form>
+      {recipes.map((recipe) => (
+        <Recipe
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients}
+        />
+      ))}
     </div>
   );
 };
